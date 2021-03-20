e7ee4a57a80538f3deb9f8f517e1a2bffcc6ae97
Gonzalo Loncaric <gonzalo.loncaric@gmail.com>
2021-03-20 12:18:43 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let nombre = prompt('Ingrese su nombre');
+console.log(nombre);
+
+alert('Bienvenido ' + nombre);
+
+alert('Vamos a solicitarte algunos datos');
+
+let edad = prompt('Ingresa tu edad');
+edad = parseInt(edad);
+let suma = edad + 9;
+
+alert('En el año 2030 vas a tener ' + suma + ' años')
+
